4a318ea8074ab822dac160dd489e15ffa2ab2b49
heba-Abdallah <heba.essam.m@hotmail.com>
2021-05-26 22:11:52 +0300

    doing backend in the lab

--- server.js
+++ server.js
@@ -2,36 +2,54 @@
 
 require('dotenv').config();
 const express = require('express');
-const cityData = require('./data/weather.json')
+const weatherData = require('./data/weather.json')
 const cors = require('cors');
-
-
 const server = express();
-server.use(cors());
-// const PORT = 3001;
 const PORT = process.env.PORT;
+
+server.use(cors());
+
+// const PORT = 3001;
 
 server.get('/', (req, res) => {
     res.send('home');
 })
 
+class Forecast {
+    constructor(item) {
+        this.date = item.valid_date;
+        this.description = item.weather.description;
+    }
+}
 
 // http://localhost:3001/getLocation?city_name=Amman
 server.get('/getLocation', (req, res) => {
     console.log(req.query);
-    let cityNameData = req.query.city_name;
-    let late=req.query.lat;
-    let long=req.query.lon;
-    let dataCity = cityData.find(item => {
-        if (item.city_name == cityNameData){
-            return item
+    let cityName = req.query.city_name;
+    // let late=req.query.lat;
+    // let long=req.query.lon;
+    let searchQuery = weatherData.find(item => {
+        if (item.city_name.toLocaleLowerCase() == cityName.toLocaleLowerCase()) {
+            return item;
         }
     })
-    res.send(dataCity);
+    console.log(searchQuery);
+
+    try {
+        let locationArr = searchQuery.data.map(item => {
+            return new Forecast(item);
+        })
+
+        res.send(locationArr);
+    }
+    catch (errors){
+        res.status(500).send('error: the informition that you searched for it are not found');
+    }
 })
 
+
 server.get('*', (req, res) => {
-    res.status(500).send('Not Found')
+    res.status(404).send('Not Found')
 })
 
 server.listen(PORT, () => {
